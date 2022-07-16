3b4a5ffa99097b8b7f4dfba790799165c0335b58
Joey <seyoung1st@gmail.com>
2022-07-16 15:01:39 +0900

    Data_type_Number

---
+++ Jstudy.js
@@ -0,0 +1,18 @@
+let myNumber1 = 0;
+let myNumber2 = 2;
+let myNumber3 = 500;
+let myNumber4 = 10000;
+
+console.log(myNumber1);
+// 0이 출력된다.
+
+console.log(myNumber1 + myNumber3);
+// 0 + 500 이므로 500이 출력된다.
+console.log(myNumber2 * myNumber3);
+// 2 * 500 이므로 1000이 출력된다.
+console.log(myNumber3 / myNumber2);
+// 500 / 2 이므로 250이 출력된다.
+console.log(myNumber3 % myNumber4);
+// 500을 10000으로 나누면 몫이 0이고 나머지가 500이므로 500이 출력된다.
+console.log(7 % 2);
+// 7을 2로 나누면 나머지가 1이므로 1이 출력된다.
